cb00ddc4b76a7a76a55e09c2e2156996f09b5425
raphaeldiscky <raph.discky@gmail.com>
2025-04-26 12:53:55 +0700

    feat: add integration test and unit test using DataJpaTest for testing `findByCode()` repository method

--- catalog-service/src/test/java/com/raphaeldiscky/bookstore/catalog/domain/ProductRepositoryTest.java
+++ catalog-service/src/test/java/com/raphaeldiscky/bookstore/catalog/domain/ProductRepositoryTest.java
@@ -2,12 +2,14 @@ package com.raphaeldiscky.bookstore.catalog.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.math.BigDecimal;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
 
+// DataJpaTest is for unit test
 @DataJpaTest(
         properties = {
             "spring.test.database.replace=none",
@@ -23,5 +25,19 @@ class ProductRepositoryTest {
     void shouldGetAllProducts() {
         List<ProductEntity> products = productRepository.findAll();
         assertThat(products).hasSize(15);
+    }
+
+    @Test
+    void shouldGetProductByCode() {
+        ProductEntity product = productRepository.findByCode("P100").orElseThrow();
+        assertThat(product.getCode()).isEqualTo("P100");
+        assertThat(product.getName()).isEqualTo("The Hunger Games");
+        assertThat(product.getDescription()).isEqualTo("Winning will make you famous. Losing means certain death...");
+        assertThat(product.getPrice()).isEqualTo(new BigDecimal("34.0"));
+    }
+
+    @Test
+    void shouldReturnEmptyWhenProductCodeNotExists() {
+        assertThat(productRepository.findByCode("invalid_product_code")).isEmpty();
     }
 }
