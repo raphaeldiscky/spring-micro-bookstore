8164a4a64b477e4b428b49413280cf9f139de3dc
raphaeldiscky <raph.discky@gmail.com>
2025-04-27 01:48:42 +0700

    fix test

--- order-service/src/test/java/com/rdisckyzp/bookstore/order/AbstractIT.java
+++ order-service/src/test/java/com/rdisckyzp/bookstore/order/AbstractIT.java
@@ -1,21 +1,20 @@
 package com.rdisckyzp.bookstore.order;
 
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
-import org.testcontainers.utility.TestcontainersConfiguration;
 
-// @NOTE: if write tests only web layer component -> use webMvcTest
-// if write tests only repository -> use DataJpaTest
-// if integration tests -> using SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 public abstract class AbstractIT {
+    // @NOTE: if write tests only web layer component -> use webMvcTest
+    // if write tests only repository -> use DataJpaTest
+    // if integration tests -> using SpringBootTest
+
     @LocalServerPort
     int port;
 
