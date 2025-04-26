6693d053596d96ec3606aa4274ec65a12174e3b6
raphaeldiscky <raph.discky@gmail.com>
2025-04-27 01:31:23 +0700

    make testcontainer config public

--- order-service/src/test/java/com/rdisckyzp/bookstore/order/TestcontainersConfiguration.java
+++ order-service/src/test/java/com/rdisckyzp/bookstore/order/TestcontainersConfiguration.java
@@ -8,7 +8,7 @@ import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
+public class TestcontainersConfiguration {
 
     @Bean
     @ServiceConnection
