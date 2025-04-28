77963f7acbb11d17991df00ef543e210ecfc359e
raphaeldiscky <raph.discky@gmail.com>
2025-04-28 22:41:36 +0700

    format

--- order-service/src/main/java/com/rdisckyzp/bookstore/order/domain/OrderService.java
+++ order-service/src/main/java/com/rdisckyzp/bookstore/order/domain/OrderService.java
@@ -4,12 +4,11 @@ import com.rdisckyzp.bookstore.order.domain.models.CreateOrderRequest;
 import com.rdisckyzp.bookstore.order.domain.models.CreateOrderResponse;
 import com.rdisckyzp.bookstore.order.domain.models.OrderCreatedEvent;
 import com.rdisckyzp.bookstore.order.domain.models.OrderStatus;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @Transactional
