e600eea9bff06081b68af340f304708d366f3a1f
raphaeldiscky <raph.discky@gmail.com>
2025-04-28 13:10:18 +0700

    feat: add circuitbreaker, timeout, retry with Resilience4j

--- order-service/src/main/java/com/rdisckyzp/bookstore/order/clients/catalog/ProductServiceClient.java
+++ order-service/src/main/java/com/rdisckyzp/bookstore/order/clients/catalog/ProductServiceClient.java
@@ -1,10 +1,13 @@
 package com.rdisckyzp.bookstore.order.clients.catalog;
 
-import java.util.Optional;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+
+import java.util.Optional;
 
 @Component
 public class ProductServiceClient {
@@ -16,18 +19,22 @@ public class ProductServiceClient {
         this.restClient = restClient;
     }
 
+
+    @CircuitBreaker(name = "catalog-service")
+    @Retry(name = "catalog-service", fallbackMethod = "getProductByCodeFallback")
     public Optional<Product> getProductByCode(String code) {
         log.info("Fetching product for code: {}", code);
-        try {
-            var product = restClient
-                    .get()
-                    .uri("/api/products/{code}", code)
-                    .retrieve()
-                    .body(Product.class);
-            return Optional.ofNullable(product);
-        } catch (Exception e) {
-            log.error("Error fetching product for code: {}", code, e);
-            return Optional.empty();
-        }
+        var product = restClient
+                .get()
+                .uri("/api/products/{code}", code)
+                .retrieve()
+                .body(Product.class);
+        return Optional.ofNullable(product);
+    }
+
+    Optional<Product> getProductByCodeFallback(String code, Throwable t) {
+        System.out.println("ProductServiceClient.getProductByCodeFallback: " + t.getMessage());
+        System.out.println("ProductServiceClient.getProductByCodeFallback: " + code);
+        return Optional.empty();
     }
 }
--- order-service/src/test/java/com/rdisckyzp/bookstore/order/web/controllers/OrderControllerTests.java
+++ order-service/src/test/java/com/rdisckyzp/bookstore/order/web/controllers/OrderControllerTests.java
@@ -18,32 +18,30 @@ class OrderControllerTests extends AbstractIT {
         void shouldCreateOrderSuccessfully() {
             var payload =
                     """
-                            {
-                                            "customer": {
-                                              "name": "Raph",
-                                              "email": "raph@gmail.com",
-                                              "phone": "9999"
-                                            },
-                                            "deliveryAddress": {
-                                              "addressLine1": "Muntilan",
-                                              "addressLine2": "Keji",
-                                              "city": "Muntilan",
-                                              "state": "Magelang",
-                                              "zipCode": "2142",
-                                              "country": "Indonesia"
-                                            },
-                                            "items": [
-                                                 {
-                                                   "code": "P100",
-                                                   "name": "Product 1",
-                                                   "price": 34.00,
-                                                   "quantity": 1
-                                                 }
-                                              ]
-                                            }
-                                          }
-
-                            """;
+                    {
+                      "customer": {
+                        "name": "Raph",
+                        "email": "raph@gmail.com",
+                        "phone": "9999"
+                      },
+                      "deliveryAddress": {
+                        "addressLine1": "Muntilan",
+                        "addressLine2": "Keji",
+                        "city": "Muntilan",
+                        "state": "Magelang",
+                        "zipCode": "2142",
+                        "country": "Indonesia"
+                      },
+                      "items": [
+                        {
+                         "code": "P100",
+                         "name": "Product 1",
+                         "price": 34.00,
+                         "quantity": 1
+                        },
+                      ]
+                    }
+                    """;
             given().contentType(ContentType.JSON)
                     .body(payload)
                     .when()
