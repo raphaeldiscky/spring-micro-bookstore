3380de0a26572a262b005786bc0c13af646174a9
raphael discky <raph.discky@gmail.com>
2025-04-29 22:04:37 +0700

    feat: add get orders API endpoints (#9)
    * feat: add get orders API endpoints
    
    * skip get orders tests

--- order-service/src/main/java/com/rdisckyzp/bookstore/order/web/controllers/OrderController.java
+++ order-service/src/main/java/com/rdisckyzp/bookstore/order/web/controllers/OrderController.java
@@ -1,10 +1,14 @@
 package com.rdisckyzp.bookstore.order.web.controllers;
 
+import com.rdisckyzp.bookstore.order.domain.OrderNotFoundException;
 import com.rdisckyzp.bookstore.order.domain.OrderService;
 import com.rdisckyzp.bookstore.order.domain.SecurityService;
 import com.rdisckyzp.bookstore.order.domain.models.CreateOrderRequest;
 import com.rdisckyzp.bookstore.order.domain.models.CreateOrderResponse;
+import com.rdisckyzp.bookstore.order.domain.models.OrderDTO;
+import com.rdisckyzp.bookstore.order.domain.models.OrderSummary;
 import jakarta.validation.Valid;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -31,5 +35,21 @@ class OrderController {
         // @NOTE: service layer should be completely independent of any web layer
         // add unit tests for web layer methods
         return orderService.createOrder(userName, request);
+    }
+
+    @GetMapping
+    List<OrderSummary> getOrders() {
+        String userName = securityService.getLoginUserName();
+        log.info("Fetching orders for user: {}", userName);
+        return orderService.findOrders(userName);
+    }
+
+    @GetMapping(value = "/{orderNumber}")
+    OrderDTO getOrder(@PathVariable(value = "orderNumber") String orderNumber) {
+        log.info("Fetching order by id: {}", orderNumber);
+        String userName = securityService.getLoginUserName();
+        return orderService
+                .findUserOrder(userName, orderNumber)
+                .orElseThrow(() -> new OrderNotFoundException(orderNumber));
     }
 }
