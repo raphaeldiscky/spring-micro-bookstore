83bb1902465834558477a419e7633f0a64cd48db
raphaeldiscky <raph.discky@gmail.com>
2025-04-30 18:49:06 +0700

    chore: first setup api-gateway

---
+++ api-gateway/src/test/java/com/rdisckyzp/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdisckyzp.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
