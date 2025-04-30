14025fd00db9fd92da0228d7bb787c3781294469
raphaeldiscky <raph.discky@gmail.com>
2025-04-30 18:07:10 +0700

    feat: add notification service with mailhog

---
+++ notification-service/src/test/java/com/rdisckyzp/bookstore/notification/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdisckyzp.bookstore.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class NotificationServiceApplicationTests extends AbstractIT {
+
+    @Test
+    void contextLoads() {}
+}
